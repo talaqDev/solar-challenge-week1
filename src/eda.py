16e185a7fabe4410327975f514d7d42c006a75c0
Hunegn <demissiehunegna@gmail.com>
2024-12-08 15:18:08 +0300

    Time series analysis

--- src/eda.py
+++ src/eda.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+ 
 
 
 def data_quality_check(data, dataset_name):
@@ -16,6 +17,18 @@ def summary_statistics(data, dataset_name):
     print(f"Summary Statistics for {dataset_name}:")
     print(data.describe())  
     print("\n")
+def time_series_analysis(data, dataset_name):
+    print(f"Plot GHI, DNI, Tamb and DHI over time for {dataset_name}:")
+    plt.figure(figsize=(14, 7))
+    plt.plot(data['GHI'], label='GHI', alpha=0.8)
+    plt.plot(data['DNI'], label='DNI', alpha=0.8)
+    plt.plot(data['DHI'], label='DHI', alpha=0.8)
+    plt.plot(data['Tamb'], label='Tamb', alpha=0.8)
+    plt.title(f'Solar Radiation Over Time {dataset_name}:')
+    plt.xlabel('Time')
+    plt.ylabel('Radiation (W/m²)')
+    plt.legend()
+    plt.show()
 
 
 def main():
@@ -26,10 +39,17 @@ def main():
     datasets = {
         "Benin": benin_data,
         "Sierra Leone": sierra_leone_data,
-        "Togo": togo_data,
+        "Togo": togo_data
     }
-
+    benin_data['Timestamp'] = pd.to_datetime(benin_data['Timestamp'])
+    benin_data.set_index('Timestamp', inplace=True)
     for name, data in datasets.items():
-        data_quality_check(data, name)
-        summary_statistics(data, name)
+        pass
+        #data_quality_check(data, name)
+        #summary_statistics(data, name)
+        time_series_analysis(data, name)
+   
+
+
+
 main()
